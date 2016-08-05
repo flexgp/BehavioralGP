767d49c2958e9d6862585f07767186c1c40346df
Steven Fine <stevenbfine92@gmail.com>
2016-08-05 08:13:27 -0700

    Change methods in Archive interface

--- src/evogpj/evaluation/Archive.java
+++ src/evogpj/evaluation/Archive.java
@@ -4,6 +4,7 @@ package evogpj.evaluation;
 import evogpj.genotype.TreeNode;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by stevenfine on 8/3/16.
@@ -11,20 +12,15 @@ import java.util.List;
 public interface Archive {
 
     /**
-     * Add a subtree to the archive.
-     * @param semantics The part of the trace corresponding to the subtree.
-     * @param syntax The TreeNode representing the subtree.
+     * Takes the genetic material, and combines it with the archive.
+     * @param geneticMaterial A list of subtrees, each represented as a map of
+     *                        its output of the training data to its syntax.
      */
-    void addSubtree(List<Double> semantics, TreeNode syntax);
+    void addGeneticMaterial(List<Map<List<Double>, TreeNode>> geneticMaterial);
 
     /**
      * Get a subtree from the archive.
      * @return The subtree that is retrieved from the archive.
      */
     TreeNode getSubtree();
-
-    /**
-     * Refresh the archive.
-     */
-    void refresh();
 }
