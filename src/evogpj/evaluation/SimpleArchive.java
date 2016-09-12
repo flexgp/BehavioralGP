fca2639e3252d894e4bde82d5e00b44e04a751f0
Steven Fine <stevenbfine92@gmail.com>
2016-09-12 08:53:48 -0400

    Change SimpleArchive to extend UnweightedArchive

--- src/evogpj/evaluation/SimpleArchive.java
+++ src/evogpj/evaluation/SimpleArchive.java
@@ -1,20 +1,16 @@
 package evogpj.evaluation;
 
 import com.google.common.collect.ImmutableList;
-import evogpj.genotype.TreeGenerator;
 import evogpj.genotype.TreeNode;
 
 import java.util.*;
 
 import evogpj.gp.MersenneTwisterFast;
-import evogpj.operator.RandomOperator;
 
 /**
  * Created by stevenfine on 8/3/16.
  */
-public class SimpleArchive extends RandomOperator implements Archive {
-
-    private Map<ImmutableList<Double>, TreeNode> archive;
+public class SimpleArchive extends UnweightedArchive {
 
     /**
      * Construct a new Archive that simply overwrites itself each time genetic
@@ -23,22 +19,9 @@ public class SimpleArchive extends RandomOperator implements Archive {
      */
     public SimpleArchive(MersenneTwisterFast rand) {
         super(rand);
-        archive = new HashMap<>();
     }
 
     public void addGeneticMaterial(Map<ImmutableList<Double>, TreeNode> geneticMaterial) {
         archive = geneticMaterial;
-    }
-
-    public TreeNode getSubtree() throws EmptyArchiveException {
-        if (archive.size() == 0) {
-            throw new EmptyArchiveException();
-        } else {
-            List<ImmutableList<Double>> keys = new ArrayList<>(archive.keySet());
-            int index = rand.nextInt(keys.size());
-            ImmutableList<Double> subtree = keys.get(index);
-            TreeNode node = archive.get(subtree);
-            return TreeGenerator.generateTree(node.toStringAsTree()).getRoot();
-        }
     }
 }
