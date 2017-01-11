de2f81593c5db8425272db373fd5c4d4e03fd758
Steven Fine <stevenbfine92@gmail.com>
2017-01-11 12:51:36 -0500

    Create ModelFitnessFunction abstract class

---
+++ src/evogpj/evaluation/java/ModelFitnessFunction.java
@@ -0,0 +1,21 @@
+package evogpj.evaluation.java;
+
+import evogpj.evaluation.BPModel;
+import evogpj.evaluation.FitnessFunction;
+
+/**
+ * Created by stevenfine on 1/11/17.
+ */
+public abstract class ModelFitnessFunction extends FitnessFunction {
+
+    protected BPModel model;
+
+    @Override
+    public Boolean requiresModel() {
+        return true;
+    }
+
+    public void setModel(BPModel model) {
+        this.model = model;
+    }
+}
