901b204b4dcfa34cac46504e49ae7b940c9adc2e
Steven Fine <stevenbfine92@gmail.com>
2017-01-11 14:06:52 -0500

    Create BP4FitnessFunctionEvaluator implementation of FitnessFunctionEvaluator

---
+++ src/evogpj/evaluation/BP4FitnessFunctionEvaluator.java
@@ -0,0 +1,45 @@
+package evogpj.evaluation;
+
+import com.google.common.collect.ImmutableList;
+import evogpj.genotype.TreeNode;
+import evogpj.gp.Population;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by stevenfine on 1/11/17.
+ */
+public class BP4FitnessFunctionEvaluator implements FitnessFunctionEvaluator {
+
+    protected List<FitnessFunction> noModelFitnessFunctions = new ArrayList<>();
+    protected List<FitnessFunction> modelFitnessFunctions = new ArrayList<>();
+    protected BPModel model;
+
+    public BP4FitnessFunctionEvaluator(
+            LinkedHashMap<String, FitnessFunction> fitnessFunctions,
+            BPModel model
+    ) {
+        this.model = model;
+        for (String name : fitnessFunctions.keySet()) {
+            FitnessFunction f = fitnessFunctions.get(name);
+            if (f.requiresModel()) {
+                modelFitnessFunctions.add(f);
+            } else {
+                noModelFitnessFunctions.add(f);
+            }
+        }
+    }
+
+    public void evalPop(Population pop) {
+        for (FitnessFunction f : noModelFitnessFunctions) {
+            f.evalPop(pop);
+        }
+        model.buildModel(pop);
+        for (FitnessFunction f: modelFitnessFunctions) {
+            f.evalPop(pop);
+        }
+    }
+}
