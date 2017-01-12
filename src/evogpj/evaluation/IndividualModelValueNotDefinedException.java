7288d4cdf77871e52334363ed03ee3ed12de3add
Steven Fine <stevenbfine92@gmail.com>
2017-01-12 13:43:30 -0500

    Create IndividualModelValueNotDefinedException class

---
+++ src/evogpj/evaluation/IndividualModelValueNotDefinedException.java
@@ -0,0 +1,11 @@
+package evogpj.evaluation;
+
+/**
+ * Created by stevenfine on 1/12/17.
+ */
+public class IndividualModelValueNotDefinedException extends Exception {
+
+    public IndividualModelValueNotDefinedException() {
+        super("Individual model value not defined.");
+    }
+}
