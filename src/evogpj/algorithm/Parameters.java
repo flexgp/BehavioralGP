f06a5912b99803d1649febf1a0ae1467a16445fd
Steven Fine <stevenbfine92@gmail.com>
2017-01-25 14:05:48 -0500

    Add FullPopulationREPTreeModel to Parameters

--- src/evogpj/algorithm/Parameters.java
+++ src/evogpj/algorithm/Parameters.java
@@ -214,6 +214,7 @@ public final class Parameters {
         public static final String REPTREE_MODEL = "fitness.REPTreeModel";
         public static final String PYTHON_MODEL = "fitness.PythonModel";
         public static final String RANDOM_MODEL = "fitness.RandomModel";
+        public static final String FULL_POP_REPTREE_MODEL = "fitness.FullPopulationREPTreeModel";
 
         // FITNESS_FUNCTION_EVALUATOR values
         public static final String ORDINARY_FITNESS_FUNCTION_EVALUATOR = "fitness.OrdinaryFitnessFunctionEvaluator";
